c872cd8ec54bad54a569df044b81c4fa47bb80c8
tetsuya <t.oba@godo-tys.jp>
2015-07-04 11:29:09 +0900

    test
    kml add

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,4 +1,4 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 //viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970,-5.730));
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://godo-tys.github.io/20150704test/cesium-starter-app_Iknowshachu\Source/20150704KML.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://godo-tys.github.io/20150704test/cesium-starter-app_Iknowshachu/Source/20150704KML.kml'));
